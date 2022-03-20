9138ab0c2c8a9dbe69c64306fd15e4b4119250b2
el bandingo <elbandingo@protonmail.com>
2022-03-20 15:22:42 -0230

    added apollo server, and updated server.js file to handle middleware. i have included the schemas requuirement, but have not yet built it. Schema to come next

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
+//database connection
 const db = require('./config/connection');
 const routes = require('./routes');
-
+//apollo server
+const { ApollopServer, ApolloServer } = require('apollo-server-express');
+//typeDefs and resolvers
+const {typeDefs, resolvers } = require('./schemas');
+//require the auth middleware
+const {authMiddleWare} = require('./utils/auth');
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleWare
+});
+
+server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,9 +29,17 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+//get all 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+  console.log(`🌍 Now listening on localhost:${PORT}`);
+  console.log(`to user GraphQl, go to http://localhost:${PORT}${server.graphqlPath}`);
+  
+  });
 });
