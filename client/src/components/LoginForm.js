72b32784e67f42ea30c46de227a4e82fd3f01f97
el bandingo <elbandingo@protonmail.com>
2022-03-20 22:04:25 -0230

    refactored login and signup form to use the mutations

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,9 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import {useMutation } from '@apollo/react-hooks';
 
-import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -26,15 +27,11 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await LOGIN_USER({
+        variables: {...userFormData }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
