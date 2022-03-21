5ecd2eb84cac0dbbecb5f581fe07282bb5090062
el bandingo <elbandingo@protonmail.com>
2022-03-20 21:34:36 -0230

    refactored code in searchbooks.js file to use mutations

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
+import { useMutation } from '@apollo/react-hooks';
+import {SAVE_BOOK} from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
+ 
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -13,6 +16,10 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+
+  //create and define save book hook from mutation
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
